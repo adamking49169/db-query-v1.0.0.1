b50cd05e76ebaa8e14301692496c4c3623b474df
adamking49169 <adamking49169@gmail.com>
2025-06-05 22:05:03 +0100

    Fixing test data for ocr for production

--- Services/OcrService.cs
+++ Services/OcrService.cs
@@ -1,13 +1,21 @@
 ﻿using HarfBuzzSharp;
+using Microsoft.Extensions.Hosting;
+using System.IO;
 using Tesseract;
 
 namespace db_query_v1._0._0._1.Services
 {
     public class OcrService
     {
+        private readonly string _tessdataPath;
+
+        public OcrService(IHostEnvironment env)
+        {
+            _tessdataPath = Path.Combine(env.ContentRootPath, "tessdata");
+        }
         public string ExtractTextFromImage(string imagePath)
         {
-            using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+            using (var engine = new TesseractEngine(_tessdataPath, "eng", EngineMode.Default))
             {
                 using (var img = Pix.LoadFromFile(imagePath))
                 {
