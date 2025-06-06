bba6ecd480eb60d78f8ca0b9a66938c9021ce34b
adamking49169 <adamking49169@gmail.com>
2025-06-06 07:32:40 +0100

    Optimising OcrService to make OCR run quicker

--- Services/OcrService.cs
+++ Services/OcrService.cs
@@ -7,23 +7,21 @@ namespace db_query_v1._0._0._1.Services
 {
     public class OcrService
     {
-        private readonly string _tessdataPath;
+        private readonly string _tessdataPath; 
+        private readonly TesseractEngine _engine;
 
         public OcrService(IHostEnvironment env)
         {
             _tessdataPath = Path.Combine(env.ContentRootPath, "tessdata");
+            _engine = new TesseractEngine(_tessdataPath, "eng", EngineMode.Default);
         }
         public string ExtractTextFromImage(string imagePath)
         {
-            using (var engine = new TesseractEngine(_tessdataPath, "eng", EngineMode.Default))
+            using (var img = Pix.LoadFromFile(imagePath))
             {
-                using (var img = Pix.LoadFromFile(imagePath))
+                using (var page = _engine.Process(img))
                 {
-                    using (var page = engine.Process(img))
-                    {
-                        var b = page.GetText();
-                        return page.GetText();
-                    }
+                    return page.GetText();
                 }
             }
         }
